30345491e39e26c0e20ac43d5b81df4e7a493341
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 13:23:12 +0000

    PostsSection.js: changed

--- src/components/PostsSection.js
+++ src/components/PostsSection.js
@@ -11,7 +11,7 @@ export default class PostsSection extends React.Component {
         const title = _.get(section, 'title');
         const subtitle = _.get(section, 'subtitle');
         const actions = _.get(section, 'actions');
-        const colNumber = _.get(page, 'col_number', 'three');
+        const colNumber = _.get(section, 'col_number', 'three');
         const posts = _.orderBy(_.get(this.props, 'posts', []), 'date', 'desc');
 
         return (
