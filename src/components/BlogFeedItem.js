7ae2dfe04500b94052a27ef589490fbebe722b65
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 13:14:54 +0000

    BlogFeedItem.js: changed

--- src/components/BlogFeedItem.js
+++ src/components/BlogFeedItem.js
@@ -25,6 +25,7 @@ export default class BlogFeedItem extends React.Component {
                     </Link>
                 )}
                 <header className="post-card__header">
+                    {subtitle && <p className="section__subtitle">{subtitle}</p>}
                     <h3 className="post-card__title"><Link href={postUrl}>{title}</Link></h3>
                     <div className="post-card__meta">
                         <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
